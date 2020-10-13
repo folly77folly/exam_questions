b930d1702f14bf01528710d5e61f945eb585b611
folly77folly <folly77folly@gmail.com>
2020-10-13 15:33:20 +0100

    added question controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/questions', 'QuestionController@create')->name('create');
+Route::get('/store', 'QuestionController@store')->name('store');
